1c22a4008b0f8c11bbd45a3d1b7cf800b99ac25f
jimlecracken14 <jimlecracken13@gmail.com>
2025-09-25 08:42:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'soutoura-frontend',
+  webDir: 'www'
+};
+
+export default config;
