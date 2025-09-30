8209b603ff19240ec5678988352f9cfabc8031db
jimlecracken14 <jimlecracken13@gmail.com>
2025-09-30 10:34:49 +0000

    ajout du composant header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IonMenuButton, IonButton, IonHeader, IonToolbar, IonTitle, IonBackButton, IonButtons } from "@ionic/angular/standalone";
-
+import { IonMenuButton, IonButton, IonHeader, IonToolbar, IonTitle, IonBackButton, IonButtons, IonContent, IonIcon } from "@ionic/angular/standalone";
+import { MenuComponent } from "../menu/menu.component";
+import { chevronBackOutline } from 'ionicons/icons';
+import {
+  addIcons
+} from "ionicons";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonButtons, IonBackButton, IonTitle, IonHeader, IonMenuButton, IonButton, IonHeader, IonToolbar, IonHeader],
+  imports: [IonIcon, IonContent, IonButtons, IonBackButton, IonTitle, IonHeader, IonMenuButton, IonButton, IonHeader, IonToolbar, IonHeader, MenuComponent],
 })
 export class HeaderComponent  implements OnInit {
   @Input() titre: string = "";
 
-  constructor() { }
+  constructor() {
+     addIcons({chevronBackOutline
+      });
+   }
 
   ngOnInit() {}
 
